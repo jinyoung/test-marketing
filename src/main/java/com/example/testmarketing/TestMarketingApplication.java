408c601106a5e61dd4357950cdc4a28931058db9
jyjang <jyjang@uengine.org>
2019-10-14 15:11:07 +0900

    first commit

---
+++ src/main/java/com/example/testmarketing/TestMarketingApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMarketingApplication.class, args);
+	}
+
+}
